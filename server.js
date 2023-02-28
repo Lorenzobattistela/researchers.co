b7f53e6ebece184a705c79565c8161829799054a
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-02-28 14:34:55 -0300

    feat: default route and removing db for deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require('express'); // Importing express module
-const db = require('./database');
+// const db = require('./database');
 const model = require('./model');
 const methods = require("./methods");
 const app = express(); // Creating an express object
@@ -24,21 +24,30 @@ app.listen(port, function (err) {
    }
 })
 
-app.get('/universities', methods.ensureToken, async function (req, res) {
-    let universities = await db.getUniversities(10);
-    let response = JSON.stringify(universities.rows);
-    res.send(response);
+app.get('/', methods.ensureToken, async function (req, res) {
+    res.send('<h1>Working api</h1>');
 });
+
+// app.get('/universities', methods.ensureToken, async function (req, res) {
+//     let universities = await db.getUniversities(10);
+//     let response = JSON.stringify(universities.rows);
+//     res.send(response);
+// });
 
 app.post('/predict', methods.ensureToken, async function (req, res) {
+   try {
     let predictMatchUniversity = await model.predictMatchUniversity((req.body))
-    res.send(predictMatchUniversity)
+    res.send(predictMatchUniversity);
+   }
+   catch(e) {
+    res.send({'error': e});
+   }
 });
 
 
 
-// curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsbyIsImVtYWlsIjoiYXNkYXMiLCJpYXQiOjE2NzczMzQ3NjR9.2Oh_3bnq5iuFYC5MaiwoAL6-aeZnhRsTnZXbGFqPp-k" --data '{ "faculdade": "engenharia química","interesses": "grafeno, química orgânica","área de pesquisa": "química, enzimas, síntese orgânica, líquidos iônicos","curso": "mestrado","estado": "RS","realocação": "true","orcamento": "3000" }' "http://localhost:8000/predict"
+// curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsbyIsImVtYWlsIjoiYXNkYXMiLCJpYXQiOjE2NzczMzQ3NjR9.2Oh_3bnq5iuFYC5MaiwoAL6-aeZnhRsTnZXbGFqPp-k" --data '{ "faculdade": "engenharia química","interesses": "grafeno, química orgânica","área de pesquisa": "química, enzimas, síntese orgânica, líquidos iônicos","curso": "mestrado","estado": "RS","realocação": "true","orcamento": "3000" }' "https://researchers-co.onrender.com/predict"
 
-// curl -X GET -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsbyIsImVtYWlsIjoiYXNkYXMiLCJpYXQiOjE2NzczMzQ3NjR9.2Oh_3bnq5iuFYC5MaiwoAL6-aeZnhRsTnZXbGFqPp-k" "http://localhost:8000/universities"
+// curl -X GET -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsbyIsImVtYWlsIjoiYXNkYXMiLCJpYXQiOjE2NzczMzQ3NjR9.2Oh_3bnq5iuFYC5MaiwoAL6-aeZnhRsTnZXbGFqPp-k" "https://researchers-co.onrender.com/predict"
 
 
